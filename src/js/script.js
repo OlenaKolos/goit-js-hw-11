40e1daf1934bcbc402dc20bd9285a89335e99b99
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-01-20 19:59:47 +0200

    add js html

--- src/js/script.js
+++ src/js/script.js
@@ -2,114 +2,97 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import alertImg from '/img/alert.svg';
 
-const formSearch = document.querySelector('.js-search');
-const listImages = document.querySelector('.gallery');
+const url = 'https://pixabay.com/api/';
+const searchParams = {
+  key: '41933677-6eee14a43ac3f23b8d77fa6f0',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 18,
+};
+
+const form = document.querySelector('.form');
+const searchTerm = document.querySelector('.searchTerm');
+const searchPhotosBtn = document.querySelector('.search-btn');
 const loader = document.querySelector('.loader');
+const gallery = document.querySelector('.gallery');
 
-loader.style.display = 'none';
-formSearch.addEventListener('submit', onSearch);
+const simpleGallery = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.8,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-function onSearch(event) {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  listImages.innerHTML = '';
-  loader.style.display = 'block';
-
-  const inputValue = event.target.elements.search.value;
-
-  getPictures(inputValue)
-    .then(data => {
-      loader.style.display = 'none';
-
-      if (!data.hits.length) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-
-      listImages.innerHTML = ('beforeend', createMarkup(data.hits));
-
-      const refreshPage = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      refreshPage.refresh();
-
-      formSearch.reset();
-    })
-    .catch(err => {
-      loader.style.display = 'none';
-      console.log(err);
-    });
-}
-
-function getPictures(name) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const KEY = '40891115-11d0b88dd3a60afc830d1d27f';
-
-  if (name.includes(' ')) {
-    name.replace(/\s+/g, '+');
+  if (searchTerm.value.trim().length < 3) {
+    showAlert('Please, enter search term!');
+    return;
   }
+  fetchPhotos()
+    .then(photos => renderPhotos(photos))
+    .catch(error => showAlert(error.toString()));
+});
 
-  const searchParams = new URLSearchParams({
-    key: KEY,
-    q: name,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  return fetch(`${BASE_URL}?${searchParams}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.statusText);
+function fetchPhotos() {
+  gallery.textContent = '';
+  showLoader(true);
+  searchParams.q = searchTerm.value.trim();
+  const searchParamsString = new URLSearchParams(searchParams);
+  return fetch(`${url}?${searchParamsString}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-    return res.json();
+    try {
+      return response.json();
+    } catch (error) {
+      throw new Error('Failed to parse JSON');
+    }
   });
 }
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<li class="gallery-item">
-          <a class="gallery-link" href="${largeImageURL}">
-            <img
-              class="gallery-image"
-              src="${webformatURL}"
-              alt="${tags}"
-              width="360"
-            />
-          </a>
-          <div class="thumb-block">
-            <div class="block">
-              <h2 class="tittle">Likes</h2>
-              <p class="amount">${likes}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Views</h2>
-              <p class="amount">${views}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Comments</h2>
-              <p class="amount">${comments}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Downloads</h2>
-              <p class="amount">${downloads}</p>
-            </div>
-          </div>
-        </li>`
-    )
+function renderPhotos(photos) {
+  if (photos.total === 0) {
+    showAlert(
+      'Sorry, there are no images matching your search query. Please try again!'
+    );
+    showLoader(false);
+    return;
+  }
+  const markup = photos.hits
+    .map(photo => {
+      return `<a class="gallery-link" href="${photo.largeImageURL}"><span class="gallery-item"><img class="gallery-image" src="${photo.webformatURL}" alt="${photo.tags}" title="${photo.tags}" /></span>
+            <span class="img-stat">
+            <span class="img-stat-item"><span class="img-stat-title">Likes</span><span class="img-stat-val">${photo.likes}</span></span>
+            <span class="img-stat-item"><span class="img-stat-title">Views</span><span class="img-stat-val">${photo.views}</span></span>
+            <span class="img-stat-item"><span class="img-stat-title">Comments</span><span class="img-stat-val">${photo.comments}</span></span>
+            <span class="img-stat-item"><span class="img-stat-title">Downloads</span><span class="img-stat-val">${photo.downloads}</span></span>
+            </span>
+            </a>`;
+    })
     .join('');
+  showLoader(false);
+  gallery.insertAdjacentHTML('beforeend', markup);
+  simpleGallery.refresh();
+}
+
+function showLoader(state = true) {
+  loader.style.display = !state ? 'none' : 'inline-block';
+  searchPhotosBtn.disabled = state;
+}
+
+function showAlert(msg) {
+  iziToast.show({
+    position: 'center',
+    iconUrl: alertImg,
+    messageColor: '#FAFAFB',
+    messageSize: '16px',
+    backgroundColor: '#EF4040',
+    close: false,
+    closeOnClick: true,
+    message: msg,
+  });
 }
